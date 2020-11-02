da3b3505c46896d78aae4b857ac68044d2015d53
Cristian Moreno <cristian.labadie@gmail.com>
2020-11-01 23:24:35 -0300

    Implementando Lodash

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,24 +1,37 @@
 import React, { useState, Fragment } from 'react';
+import _ from 'lodash';
+
 
 const Contador = () => {
-    const[ count, setCount ] = useState(1);
+    const [ count, setCount ] = useState(1);
 
-    const aumentar = () => {
-        setCount(count +1)
-        console.log(`Me diste ${count}`);
-    }  
+    const [ numeros, setNumeros ] = useState([1,2,3,4,5,6,7,8,9]);
 
-    const resetCount = () => {
-        setCount(0)
+    const pushNumero = () => {
+        let lastIndexValue = _.last(numeros)
+
+
+        setNumeros([
+            ...numeros, lastIndexValue + 1
+        ])
+
+
+        
     }
 
     return (  
-        <Fragment>
-            <p>Cantidad de click {count} </p>
-            <button onClick={aumentar}>
-                Clickeame 
-            </button>
-            <button onClick={resetCount}>Resetear contador</button>
+        <Fragment>            
+            <button onClick={pushNumero}>Push</button>
+
+            <ul>
+                {
+                    numeros.map( (e, index) => {
+                       return <li key={index}>
+                             {e}
+                        </li>
+                    })
+                }
+            </ul>
         </Fragment>
      );
 }
